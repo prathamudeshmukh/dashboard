7ff554304f10c4446898688d2e94fe211a2c104e
Rohan Patil <rohanpatil7778@gmail.com>
2025-01-31 15:24:56 +0530

    fix: fix build command

--- src/libs/DB.ts
+++ src/libs/DB.ts
@@ -1,11 +1,14 @@
 import { Pool } from '@neondatabase/serverless';
+import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/neon-serverless';
 
 import * as schema from '@/models/Schema';
 
-import { Env } from './Env';
+config({ path: '.env' });
 
-const pool = new Pool({ connectionString: Env.DATABASE_URL });
+const pool = new Pool({ connectionString: process.env.DATABASE_URL!, ssl: {
+  rejectUnauthorized: false,
+} });
 const client = await pool.connect();
 
 export const db = drizzle(client, { schema });
