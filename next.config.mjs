249fe992c8d5864bc3df7d06a59ad6ea7a5f48c2
prathamesh <prathamudeshmukh@gmail.com>
2025-03-02 16:44:25 +0530

    feat: config sentry

--- next.config.mjs
+++ next.config.mjs
@@ -33,8 +33,8 @@ export default withSentryConfig(
     // For all available options, see:
     // https://github.com/getsentry/sentry-webpack-plugin#options
     // FIXME: Add your Sentry organization and project names
-    org: 'nextjs-boilerplate-org',
-    project: 'nextjs-boilerplate',
+    org: 'templify',
+    project: 'javascript-nextjs',
 
     // Only print logs for uploading source maps in CI
     silent: !process.env.CI,
