e7c481d07322ec5bd1b53fa26bf78574411463bc
Rohan Patil <rohanpatil7778@gmail.com>
2025-04-02 15:18:54 +0530

    feat: add setting opt to show api keys

--- src/features/dashboard/DashboardContent.tsx
+++ src/features/dashboard/DashboardContent.tsx
@@ -1,20 +1,16 @@
-import { auth } from '@clerk/nextjs/server';
 import Link from 'next/link';
 import { useTranslations } from 'next-intl';
 import React from 'react';
 
 import { Button } from '@/components/ui/button';
 
-import { ShowClientSecret } from './ShowClientSecret';
 import TemplateTable from './TemplateTable';
 
 export const DashboardContent = () => {
   const t = useTranslations('DashboardContent');
-  const { userId } = auth();
   return (
     <div className="flex  flex-col rounded-2xl bg-card p-5 shadow-2xl">
       <div className="mt-3 text-center">
-        <ShowClientSecret clientId={userId as string} />
         <div className="text-xl font-semibold">{t('title')}</div>
       </div>
       <div className="mt-5 flex items-end justify-end">
