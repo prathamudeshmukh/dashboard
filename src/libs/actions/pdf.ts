21211d9ed30ecef636767b1fc1ef73ca83fbef8a
Rohan Patil <rohanpatil7778@gmail.com>
2025-04-22 12:58:52 +0530

    fix: fix inngest base url

--- src/libs/actions/pdf.ts
+++ src/libs/actions/pdf.ts
@@ -30,13 +30,13 @@ export async function uploadPdf(formData: FormData) {
 }
 
 export async function getStatus(runId: string) {
-  const response = await fetch(`${inngestBaseUrl}/v1/events/${runId}/runs`, {
+  const response = await fetch(`${inngestBaseUrl}v1/events/${runId}/runs`, {
     headers: {
       Authorization: `Bearer ${process.env.INNGEST_SIGNING_KEY}`,
     },
   });
   // eslint-disable-next-line no-console
-  console.log('Inngest URL:', `${inngestBaseUrl}/v1/events/${runId}/runs`);
+  console.log('Inngest URL:', `${inngestBaseUrl}v1/events/${runId}/runs`);
   const rawText = await response.text();
 
   if (!response.ok) {
