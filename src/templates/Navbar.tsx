f4aa3219ed26f80ad20f8d96647a9fd524dfcd96
Rohan Patil <rohanpatil7778@gmail.com>
2025-05-09 10:14:38 +0530

    fix: fix login flow and ui

--- src/templates/Navbar.tsx
+++ src/templates/Navbar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ClerkProvider, SignInButton, SignUpButton } from '@clerk/nextjs';
+import { SignOutButton, SignUpButton, useUser } from '@clerk/nextjs';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { useTranslations } from 'next-intl';
@@ -12,43 +12,52 @@ import type { NavbarProps } from '@/types/Navbar';
 import { Logo } from './Logo';
 
 export const Navbar = ({ menuList, basePath = ' ' }: NavbarProps) => {
+  const { isSignedIn } = useUser();
   const t = useTranslations('Navbar');
   const pathname = usePathname();
   const isLandingPage = pathname === '/';
 
   return (
-    <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <section className="px-12 py-6">
-        <CenteredMenu
-          logo={<Logo />}
-          rightMenu={(
-            <ClerkProvider>
-              <li className="ml-1 mr-2.5" data-fade>
-                <SignInButton mode="modal" forceRedirectUrl="/dashboard">
-                  <Button size="sm" variant="outline">{t('sign_in')}</Button>
-                </SignInButton>
-              </li>
+    <>
+      <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <section className="px-12 py-6">
+          <CenteredMenu
+            logo={<Logo />}
+            rightMenu={(
               <li>
-                <SignUpButton mode="modal" forceRedirectUrl="/dashboard">
-                  <Button size="sm">{t('sign_up')}</Button>
-                </SignUpButton>
+                {isSignedIn
+                  ? (
+                      <div className="flex justify-end space-x-2 py-2">
+                        <SignOutButton>
+                          <Button variant="ghost" size="sm">Sign Out</Button>
+                        </SignOutButton>
+                        <Link href="/dashboard">
+                          <Button size="sm" variant="default">Dashboard</Button>
+                        </Link>
+                      </div>
+                    )
+                  : (
+                      <SignUpButton mode="modal" forceRedirectUrl="/dashboard">
+                        <Button size="sm">{t('sign_up')}</Button>
+                      </SignUpButton>
+                    )}
               </li>
-            </ClerkProvider>
-          )}
-        >
-          {menuList.map((item, index) => {
-            const linkHref = isLandingPage ? item.link : `${basePath}${item.link.replace('#', '/')}`;
+            )}
+          >
+            {menuList.map((item, index) => {
+              const linkHref = isLandingPage ? item.link : `${basePath}${item.link.replace('#', '/')}`;
 
-            return (
-              <li key={index}>
-                <Link href={linkHref}>
-                  {item.name}
-                </Link>
-              </li>
-            );
-          })}
-        </CenteredMenu>
-      </section>
-    </header>
+              return (
+                <li key={index}>
+                  <Link href={linkHref}>
+                    {item.name}
+                  </Link>
+                </li>
+              );
+            })}
+          </CenteredMenu>
+        </section>
+      </header>
+    </>
   );
 };
