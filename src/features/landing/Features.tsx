f043e851a76de52fe1c42d595d5609d741c58296
Rohan Patil <rohanpatil7778@gmail.com>
2025-07-12 14:10:30 +0530

    fix: update landing page copy for feature section

--- src/features/landing/Features.tsx
+++ src/features/landing/Features.tsx
@@ -6,48 +6,17 @@ import { useTranslations } from 'next-intl';
 import { FeatureAccordionItem } from '@/components/landing/FeatureAccordionItem';
 import { Accordion } from '@/components/ui/accordion';
 import { Button } from '@/components/ui/button';
+import { useFeatures } from '@/hooks/UseFeatures';
 
 export default function Features() {
   const t = useTranslations('Features');
-  const features = [
-    {
-      id: t('id_1'),
-      title: t('title_1'),
-      content: t('content_1'),
-    },
-    {
-      id: t('id_2'),
-      title: t('title_2'),
-      content: t('content_2'),
-    },
-    {
-      id: t('id_3'),
-      title: t('title_3'),
-      content: t('content_3'),
-    },
-    {
-      id: t('id_4'),
-      title: t('title_4'),
-      content: t('content_4'),
-    },
-    {
-      id: t('id_5'),
-      title: t('title_5'),
-      content: t('content_5'),
-    },
-    {
-      id: t('id_6'),
-      title: t('title_6'),
-      content: t('content_6'),
-    },
-  ];
+  const features = useFeatures();
 
   return (
     <section id="features" className="bg-white py-20">
       <div className="container px-5 md:px-0">
         <h2 className="text-center text-4xl font-bold md:text-5xl lg:text-6xl lg:font-semibold">{t('title')}</h2>
         <p className="mx-auto mb-6 mt-4 max-w-3xl text-center font-normal text-gray-600">{t('subtitle')}</p>
-
         <div className="mb-12 flex justify-center">
           <Button className="rounded-full bg-primary text-lg font-normal hover:bg-primary">{t('cta')}</Button>
         </div>
@@ -60,6 +29,7 @@ export default function Features() {
                   key={feature.id}
                   title={feature.title}
                   description={feature.content}
+                  points={feature.points}
                   value={feature.id}
                 />
               ))}
