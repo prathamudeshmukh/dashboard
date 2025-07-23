fe18a3068d1f4aed191c716e051af8beb9020bc2
prathamesh <prathamudeshmukh@gmail.com>
2025-07-23 15:01:45 +0530

    fix: upload xml report to github

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   // Reporter to use. See https://playwright.dev/docs/test-reporters
   reporter: process.env.CI
-    ? [['github'], ['html', { outputFolder: 'playwright-report', open: 'never' }]]
+    ? [['list'], ['github'], ['html', { outputFolder: 'playwright-report', open: 'never' }], ['junit', { outputFile: 'results.xml' }]]
     : 'list',
 
   expect: {
