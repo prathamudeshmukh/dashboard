cab73cdbbc7d53c24ead436611159985a6fe1dcc
Rohan Patil <rohanpatil7778@gmail.com>
2025-08-16 12:52:54 +0530

    test: add logs

--- src/inngest/functions/extractPdf/convertToHtml.ts
+++ src/inngest/functions/extractPdf/convertToHtml.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 export async function convertToHTML(downloadUrl: string, logger: any) {
   try {
     const baseUrl = process.env.JOB_RUNNER_BASE_URL;
+    logger.info('Base URL is', baseUrl);
     const token = process.env.JOB_RUNNER_TOKEN;
     logger.info('Extracting html from pdf');
     const response = await axios.post(
@@ -15,6 +16,7 @@ export async function convertToHTML(downloadUrl: string, logger: any) {
         },
       },
     );
+    logger.info('Response', response);
     return response.data.html;
   } catch (error) {
     console.error('Error Fetching Extracted PDF content:', error);
