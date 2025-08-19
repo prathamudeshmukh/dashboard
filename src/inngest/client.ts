14549f8f8abb1b6f61baac032cbbef2f6f11b208
prathamesh <prathamudeshmukh@gmail.com>
2025-08-19 16:36:16 +0530

    feat: add script for builds

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -1,4 +1,7 @@
 import { Inngest } from 'inngest';
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: 'templify-app', env: process.env.VERCEL_GIT_COMMIT_REF || 'develop' });
+export const inngest = new Inngest({
+  id: 'templify-app',
+  env: process.env.INNGEST_ENV || process.env.VERCEL_GIT_COMMIT_REF || 'develop',
+});
