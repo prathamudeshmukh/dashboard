5e2334cc427f4f69828022aa4bb8830945c84845
Rohan Patil <rohanpatil7778@gmail.com>
2025-09-10 12:35:50 +0530

    fix: issue in edit template for visual editor

--- src/components/template/html-builder/LoadTemplateContent.ts
+++ src/components/template/html-builder/LoadTemplateContent.ts
@@ -1,10 +1,10 @@
-import type { Editor } from 'grapesjs';
+import type { TemplateEditorApi } from 'grapesjs-hbs-react';
 
 import { fetchTemplateById } from '@/libs/actions/templates';
 import type { CreationMethodEnum } from '@/types/Enum';
 
 export async function loadTemplateContent({
-  editor,
+  editorApi,
   templateId,
   setHtmlContent,
   setHtmlTemplateJson,
@@ -13,7 +13,7 @@ export async function loadTemplateContent({
   setTemplateName,
   setTemplateDescription,
 }: {
-  editor: Editor;
+  editorApi: TemplateEditorApi;
   templateId: string | null;
   setHtmlContent: (val: string) => void;
   setHtmlStyle: (val: string) => void;
@@ -22,30 +22,32 @@ export async function loadTemplateContent({
   setHtmlTemplateJson: (val: string) => void;
   setTemplateDescription: (val: string) => void;
 }) {
-  if (templateId) {
-    try {
-      const response = await fetchTemplateById(templateId);
-      if (!response?.data) {
-        return;
-      }
-
-      const content = response.data.templateContent as string;
-      const style = response.data.templateStyle as string;
-
-      setHtmlContent(content);
-      setHtmlStyle(style);
-      setHtmlTemplateJson(JSON.stringify(response.data.templateSampleData));
-      setCreationMethod(response.data.creationMethod as CreationMethodEnum);
-      setTemplateName(response.data.templateName as string);
-      setTemplateDescription(response.data.description as string);
-      if (content) {
-        editor?.setComponents(content);
-      }
-      if (style) {
-        editor.setStyle(style);
-      }
-    } catch (error) {
-      console.error('Failed to load template for editing:', error);
+  if (!templateId) {
+    return;
+  }
+  try {
+    const response = await fetchTemplateById(templateId);
+    if (!response?.data) {
+      return;
     }
+
+    const content = response.data.templateContent as string;
+    const style = response.data.templateStyle as string;
+    const dataSource = JSON.stringify(response.data.templateSampleData);
+    const creationMethod = response.data.creationMethod as CreationMethodEnum;
+    const templateName = response.data.templateName as string;
+    const templateDescription = response.data.description as string;
+
+    setHtmlContent(content);
+    setHtmlStyle(style);
+    setHtmlTemplateJson(dataSource);
+    setCreationMethod(creationMethod);
+    setTemplateName(templateName);
+    setTemplateDescription(templateDescription);
+
+    editorApi.clear();
+    editorApi.loadTemplate(content, JSON.parse(dataSource), JSON.parse(dataSource), style);
+  } catch (error) {
+    console.error('Failed to load template for editing:', error);
   }
 }
