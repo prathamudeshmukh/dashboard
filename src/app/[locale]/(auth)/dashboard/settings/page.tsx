e27989303d44c809e5ddded8e7f5524d9427dd9e
Rohan Patil <rohanpatil7778@gmail.com>
2025-11-15 11:36:26 +0530

    fix: remove webhook ui component temporary

--- src/app/[locale]/(auth)/dashboard/settings/page.tsx
+++ src/app/[locale]/(auth)/dashboard/settings/page.tsx
@@ -2,14 +2,12 @@ import { auth } from '@clerk/nextjs/server';
 import React from 'react';
 
 import APIKeys from '@/features/dashboard/settings/APIKeys';
-import WebhooksPage from '@/features/dashboard/settings/Webhooks';
 
 const page = () => {
   const { userId } = auth();
   return (
     <div className="space-y-12">
       <APIKeys clientId={userId as string} />
-      <WebhooksPage />
     </div>
   );
 };
